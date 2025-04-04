90b2b4a5e596b889db9c6ae8b01ad5d290396697
“kevin” <kevinle1878@gmail.com>
2025-04-03 21:55:11 -0700

    Setting up Client

---
+++ client/client.go
@@ -0,0 +1,38 @@
+package client
+
+import "bytes"
+
+const defaultScratchsize = 64 * 1024
+
+type Simple struct {
+	addrs []string
+	buf   bytes.Buffer
+}
+
+// Creating a new client
+func NewSimple(addrs []string) *Simple {
+	return &Simple{
+		addrs: addrs,
+	}
+
+}
+
+// Sends msgs to server
+func (s *Simple) Send(msgs []byte) error {
+	_, err := s.buf.Write(msgs)
+	return err
+}
+
+// Receive data from server
+// wait for new msgs or return an error in case something goes wrong
+func (s *Simple) Receive(scratch []byte) ([]byte, error) {
+	if scratch == nil {
+		scratch = make([]byte, defaultScratchsize)
+	}
+	n, err := s.buf.Read(scratch)
+	if err != nil {
+		return nil, err
+	}
+	// return data and nil for error
+	return scratch[0:n], nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/2-coffee/Distributed-Logs
+
+go 1.24.2
