c6e3da47eb68f548f06597efac87088b2d5a0020
“kevin” <kevinle1878@gmail.com>
2025-04-05 08:37:53 -0700

    some client testing

--- client/client.go
+++ client/client.go
@@ -1,12 +1,18 @@
 package client
 
-import "bytes"
+import (
+	"bytes"
+	"errors"
+)
 
 const defaultScratchsize = 64 * 1024
 
+var errBufferTooSmall = errors.New("buffer is too small to fit one message")
+
 type Simple struct {
-	addrs []string
-	buf   bytes.Buffer
+	addrs         []string
+	buf           bytes.Buffer
+	restOfLastbuf bytes.Buffer
 }
 
 // Creating a new client
@@ -27,12 +33,53 @@ func (s *Simple) Send(msgs []byte) error {
 // wait for new msgs or return an error in case something goes wrong
 func (s *Simple) Receive(scratch []byte) ([]byte, error) {
 	if scratch == nil {
-		scratch = make([]byte, defaultScratchsize) // making a buffer incase we are waiting
+		scratch = make([]byte, defaultScratchsize) // making a buffer
 	}
-	n, err := s.buf.Read(scratch)
-	if err != nil { // error in Read
+	offset := 0
+
+	if s.restOfLastbuf.Len() > 0 {
+		if s.restOfLastbuf.Len() >= len(scratch) {
+			return nil, errBufferTooSmall
+		}
+		n, err := s.restOfLastbuf.Read(scratch) // read the last message that got cut off into buffer
+		if err != nil {
+			return nil, err
+		}
+		offset += n
+
+	}
+
+	n, err := s.buf.Read(scratch[offset:]) // read into buffer
+	if err != nil {                        // error in Read
 		return nil, err
 	}
-	// return data and nil for error
-	return scratch[0:n], nil
+	// fmt.Println("\n\n")
+	// fmt.Println(scratch[0:n])
+
+	// one of the problems I ran into is that read would get cut off
+	truncated, rest, err := splitLastMessage(scratch[0 : n+offset])
+
+	s.restOfLastbuf.Reset()
+	s.restOfLastbuf.Write(rest)
+	if err != nil {
+		return nil, err
+	}
+
+	return truncated, nil
+}
+
+func splitLastMessage(res []byte) (truncated []byte, rest []byte, err error) {
+	n := len(res)
+	if len(res) == 0 {
+		return res, nil, nil
+	}
+	if res[n-1] == '\n' { // all logs read
+		return res, nil, nil
+	}
+	lastLog := bytes.LastIndexByte(res, '\n')
+	if lastLog < 0 {
+		return nil, nil, errBufferTooSmall
+	}
+	lastLog += 1
+	return res[0:lastLog], res[lastLog:], nil
 }
---
+++ client/client_test.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestSplitLastMessage(t *testing.T) {
+	res := []byte("100\n101\n10")
+	testTruncated, testRest := []byte("100\n101\n"), []byte("10")
+
+	Truncated1, Rest1, err := splitLastMessage(res)
+	if err != nil {
+		t.Errorf("splitLastMessage(%q): got error %v", string(res), err)
+	}
+	if !bytes.Equal(testTruncated, Truncated1) || !bytes.Equal(testRest, Rest1) {
+		t.Errorf("splitLastMessage(%q): got %q, %q; wanted %q, %q", string(res), string(Truncated1), string(Rest1), string(testTruncated), string(testRest))
+	}
+}
+
+// could not get full message
+func TestSplitLastMessageSmallBuffer(t *testing.T) {
+	res := []byte("10010110")
+	_, _, err := splitLastMessage(res)
+	if err == nil {
+		t.Errorf("splitLastMessage(%q): did not get an error, want an error here.", string(res))
+	}
+}
